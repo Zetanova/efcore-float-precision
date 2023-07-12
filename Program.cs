ebdde9839e032b9dae0b132ae3efb8d20c063b05
zetanova <dirnberger@cwtech.at>
2023-07-12 11:33:18 +0200

    add cast issue test

--- Program.cs
+++ Program.cs
@@ -28,10 +28,11 @@ namespace efcore_float_precision
 
             var test1 = new TestEntity
             {
-                Value = 1.03d
+                Value1 = 1.03d,
+                Value2 = 1.04d,
             };
 
-            Console.WriteLine($"value {test1.Value} saving");
+            Console.WriteLine($"value {test1.Value1} saving");
 
             context.Tests.Add(test1);
 
@@ -43,17 +44,16 @@ namespace efcore_float_precision
 
             var test1Loaded = await context.Tests.FindAsync(new object[] { test1.Id }, cts.Token);
 
-            Console.WriteLine($"value {test1Loaded!.Value} loaded");
+            Console.WriteLine($"value {test1Loaded!.Value1} loaded");
 
 
             //set value to source
-            test1Loaded.Value = test1.Value;
+            test1Loaded.Value1 = test1.Value1;
 
             context.ChangeTracker.DetectChanges();
 
             if(context.ChangeTracker.HasChanges())
-                Console.WriteLine($"value changed between round trip");
-
+                Console.Error.WriteLine($"value changed between round trip");
 
             //result output
             /*
@@ -61,6 +61,47 @@ namespace efcore_float_precision
                 value 1,0299999713897705 loaded
                 value changed between round trip
             */
+
+            {
+                Console.WriteLine("reading nullable value2 over projection without default");
+                var items = await context.Tests.AsNoTracking()
+                    .Select(n => new TestDto
+                    {
+                        Value = n.Value2
+                    })
+                    .ToListAsync(cts.Token);
+            }
+
+            {
+                Console.WriteLine("reading nullable value over projection with default");
+                var items = await context.Tests.AsNoTracking()
+                    .Select(n => new TestDto
+                    {
+                        Value = n.Value2 ?? 0
+                    })
+                    .ToListAsync(cts.Token);
+
+                /*
+                 System.InvalidOperationException
+                      HResult=0x80131509
+                      Message=An error occurred while reading a database value. The expected type was 'System.Nullable`1[System.Double]' but the actual value was of type 'System.Double'.
+                      Source=Microsoft.EntityFrameworkCore.Relational
+                      StackTrace:
+                       at Microsoft.EntityFrameworkCore.Query.RelationalShapedQueryCompilingExpressionVisitor.ShaperProcessingExpressionVisitor.ThrowReadValueException[TValue](Exception exception, Object value, Type expectedType, IPropertyBase property)
+                       at Microsoft.EntityFrameworkCore.Query.Internal.SingleQueryingEnumerable`1.AsyncEnumerator.<MoveNextAsync>d__20.MoveNext()
+                       at Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.<ToListAsync>d__65`1.MoveNext()
+                       at Microsoft.EntityFrameworkCore.EntityFrameworkQueryableExtensions.<ToListAsync>d__65`1.MoveNext()
+                       at efcore_float_precision.Program.<Main>d__0.MoveNext() in M:\projects\efcore-float-precision\Program.cs:line 77
+                       at efcore_float_precision.Program.<Main>d__0.MoveNext() in M:\projects\efcore-float-precision\Program.cs:line 83
+
+                      This exception was originally thrown at this call stack:
+                        [External Code]
+
+                    Inner Exception 1:
+                    InvalidCastException: Unable to cast object of type 'System.Double' to type 'System.Single'.
+
+                */
+            }
         }
     }
 
@@ -83,6 +124,14 @@ namespace efcore_float_precision
         public int Id { get; set; }
 
         [Precision(2)]
-        public double Value { get; set; }
+        public double Value1 { get; set; }
+
+        [Precision(2)]
+        public double? Value2 { get; set; } //nullable
+    }
+
+    public sealed class TestDto
+    {
+        public double? Value { get; set; }
     }
 }
